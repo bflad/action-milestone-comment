d5ec1c38bfb31df2711db992ad5dce6471bcc480
Brian Flad <bflad417@gmail.com>
2024-05-09 17:07:58 -0400

    Updates to switch ESLint to flat config and resolve new reports

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const js = require('@eslint/js');
+const jest = require('eslint-plugin-jest');
+
+module.exports = [
+    {
+        ignores: ["dist/**", "node_modules/**"],
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+        },
+    },
+    {
+        files: ["**/*.test.js"],
+        languageOptions: {
+            sourceType: "commonjs",
+        },
+        plugins: {
+            jest: jest,
+        },
+        rules: {
+            ...jest.configs["flat/recommended"].rules,
+        },
+    },
+];
